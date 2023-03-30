787ec81e4d1dfb8583576ebde7ab5483c5ee6012
Donnacha <donnacha.mcgrath4@gmail.com>
2023-03-30 15:19:26 +1100

    Remove image size

--- main.py
+++ main.py
@@ -4,4 +4,4 @@ st.set_page_config("Lara", layout="wide")
 
 st.header("Lara is awesome")
 st.write("Just thought I'd make a webpage to try and make you smile.")
-st.image("resources/110422-stop-caring.png", width=500)
+st.image("resources/110422-stop-caring.png")
