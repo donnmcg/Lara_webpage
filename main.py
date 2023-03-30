2a79bce0ccb7917dd8fef707f1eb959f41d6aa32
Donnacha <donnacha.mcgrath4@gmail.com>
2023-03-30 15:13:31 +1100

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.set_page_config("Lara", layout="wide")
+
+st.header("Lara is awesome")
+st.write("Just thought I'd make a webpage to try and make you smile.")
+st.image("resources/110422-stop-caring.png", width=500)
